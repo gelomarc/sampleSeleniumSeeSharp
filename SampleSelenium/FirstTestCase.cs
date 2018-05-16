193486db1ee8d99ddf137c23e6b7e0099098ed1e
Gelo <gelomarc@gmail.com>
2018-05-16 10:38:23 +0200

    Add project files.

---
+++ SampleSelenium/FirstTestCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSelenium
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://automationpractice.com";
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
