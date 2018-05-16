4a5502145e503ac703471caad4b28de337706a24
Gelo <gelomarc@gmail.com>
2018-05-16 12:23:22 +0200

    First test

---
+++ SampleSelenium/PageObjects/BasePage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSelenium.PageObjects
+{
+    class BasePage
+    {
+
+        protected IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "contact-link")]
+        public IWebElement contactLinkButton { get; set; }
+    }
+}
---
+++ SampleSelenium/Test.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SampleSelenium.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSelenium
+{
+    class Test
+    {
+        IWebDriver driver;
+        BasePage basePage;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            basePage = new BasePage(driver);
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "http://automationpractice.com";
+            basePage.contactLinkButton.Click();
+        }
+    
+        [TearDown]
+        public void EndTesT()
+        {
+            driver.Close();
+        }
+    }
+}
