00eaeedc548a979874e77dd3fabd54ed13100b0a
Gelo <gelomarc@gmail.com>
2018-05-20 21:58:53 +0200

    Add Page Object

---
+++ SampleSelenium/PageObjects/HomePage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSelenium.PageObjects
+{
+    public class BrowseInventoryPage : BasePage
+    {
+        public BrowseInventoryPage(IWebDriver driver) : base(driver)
+        {}
+
+        IReadOnlyCollection<IWebElement> productList;
+
+        public String getInventoryItemName(int index)
+        {
+            setProductList();
+            return productList.ElementAt(index).FindElement(By.XPath(".//a[@class = 'product-name']")).Text;
+        }
+
+        public String getInventoryItemPrice(int index)
+        {
+            setProductList();
+            return productList.ElementAt(index).FindElement(By.XPath(".//div[@class= 'right-block']//span[@class = 'price product-price']")).Text.Substring(1);
+        }
+
+        private void setProductList()
+        {
+            productList = driver.FindElements(By.XPath("//ul[starts-with(@class, 'product_list')]/li"));
+        }
+
+    }
+}
